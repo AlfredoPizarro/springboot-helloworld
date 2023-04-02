3dc09de174c520444fccc72a79123b18acb6e86e
Alfredo <apizarro@company.com>
2023-04-02 15:11:08 -0400

    First commit

--- src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
+++ src/test/java/com/example/helloworld/controller/HelloWorldControllerTest.java
@@ -22,6 +22,7 @@ public class HelloWorldControllerTest {
 
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("Hello, World!"));
+                .andExpect(content().string("Hello, World!"))
+                .andExpect(content().string("Test"));
     }
 }
